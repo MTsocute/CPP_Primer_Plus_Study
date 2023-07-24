a814073564ba810e153c74dbf46cec2137eb0b22
Momo <75737534+MTsocute@users.noreply.github.com>
2023-07-24 12:16:21 +0800

    刷新对 getline() 和 get() 的认识，以及造成的缓存区问题的了解

---
+++ Chapter4/Source/字符串获取存在的问题.cpp
@@ -0,0 +1,75 @@
+//
+// Created by Momo on 2023/6/24.
+//
+#include <iostream>
+
+using namespace std;
+
+// cin >> 给数字赋值遗留的 '\n' 问题
+void cin_problem() {
+    cout << "When did you build your house ?\n";
+    int year;
+    cin >> year;    // 这里有一个问题，就是读取了数字以后，在缓存区保留了 '\n'
+
+    cout << "And where is your house? \n";
+    char location[20];
+    // 缓存区还有一个换行符号，然后 getline() 读取缓存区的时候就读到了这个
+    // 缓存区前的内容啥也没有就是空字符了
+    cin.getline(location, 20);
+
+    cout << "The year you built your house: " << year << endl;
+    cout << "The address of  your house: " << location << endl;
+    puts("Done!");
+}
+
+// cin.get() 给字符串赋值遗留 '\n' 在缓存区的 或 长度超出字符串的范围的问题
+void cin_string_problem() {
+    char a[5];
+    char b[5];
+    /* 1. 输入短少的字符，会让第一个 cin.get() 读取到 '\n' 停止，然后 '\n' 留在缓存区
+     * 2. 输入的字符过多，溢出，但是没有清理缓存区，下一次 cin.get() 直接读取缓存区的内容
+     * */
+    cin.get(a, 5);
+    cin.get(b, 5);
+
+    cout << a << endl;
+    cout << b << endl;
+    cout << "Done\n";
+}
+
+// cin.getline() 只有长度溢出影响下一次的问题
+void cin_string_problem_2() {
+    char a[5];
+    char b[5];
+    /* 输入的字符过多，溢出，但是没有清理缓存区，下一次 cin 直接读取缓存区的内容，跳过读取
+     * */
+    cin.getline(a, 5);
+    cin.get(b, 5);
+
+    cout << a << endl;
+    cout << b << endl;
+    cout << "Done\n";
+}
+
+
+// 使用 cin.ignore() 清理缓存区多余数据的
+void clean_cache() {
+    char a[5];
+    char b[5];
+    /* 输入的字符过多，溢出，但是没有清理缓存区，
+     * 下一次 cin 直接读取缓存区的内容，跳过读取 */
+    cin.get(a, 5);
+    cin.ignore(1000, '\n');
+//    cin.ignore();
+    cin.getline(b, 5);
+
+    cout << a << endl;
+    cout << b << endl;
+    cout << "Done\n";
+}
+
+int main() {
+    clean_cache();
+
+    return 0;
+}
--- Chapter4/Source/读取一行.cpp
+++ Chapter4/Source/读取一行.cpp
@@ -6,22 +6,26 @@
 
 using namespace std;
 
-// getline() 函数的使用
+/**
+ * 两个函数都停止在 '\n'
+    * 但是 getline() 如果不超过指定数量，就会包括结尾的 '\n'，如果超过最后一个位置的 '\n' 会被替换成 '\0'
+    * get() 就是默认在 '\n' 停止，但是不读取，所以'\n' 会遗留在缓冲区造成问题
+ * 所以两个函数在一缓存区溢出问题上各有千秋，我们具体看 字符串获取存在问题.cpp 中这个文件了解详情
+ * */
+
+
+// 使用getline()读取一整行，包括空格和换行符
 void use_get_line() {
     char arr[20];
     cout << "Input a name plz: \n";
 
-    /* 向数组中输入字符串
-         * 用 \n 确定结尾位置
-         * 用空白字符替换结尾，所以不包括换行符在字符中
-     * */
     cin.getline(arr, 20);
     cout << "Your name is :" << arr << endl;
 
     puts("End~~~~");
 }
 
-// get() 方法会保留结尾的换行
+// 使用get()读取指定数量数据，不包括换行符
 void use_get() {
     char input[256];
 
